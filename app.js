dfe95e372e322ee5754d1a2ec00117403b6d77f4
laoluwapo <alividegabriel9@gmail.com>
2023-03-25 19:59:43 +0100

    Get the other profile data from the API and display them on the webpage.

--- app.js
+++ app.js
@@ -22,3 +22,58 @@ const getUserData = async () => {
     searchInput.value = "";
   }
 };
+
+// Function that checks for the user's data
+async function searchUser() {
+  try {
+    const data = await getUserData();
+    getDate(data);
+    getProfileData(data);
+    console.log(data);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+// Function that gets the date
+function getDate(data) {
+  // Input date string from API
+  const apiDateStr = data.created_at;
+  // Create a date object from the input string
+  const apiDate = new Date(apiDateStr);
+  // Create an option for formatting the date
+  const options = {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  };
+  // Format the date to the desired output format
+  const outputDateStr = apiDate.toLocaleDateString("en-NG", options);
+  const date = document.querySelector(".profile-date");
+  const displayDate = "Joined " + outputDateStr;
+  date.textContent = displayDate;
+}
+
+// Function that gets other details of the user
+function getProfileData(data) {
+  const username = document.querySelector(".profile-name");
+  const profileUsername = document.querySelector(".profile-username");
+  const profilePicture = document.querySelector(".dp-wrapper img");
+  const userBio = document.querySelector(".profile-desc");
+  username.textContent = data.name;
+  profileUsername.textContent = "@" + data.login;
+  // Get image url from api and assign it to the profile img
+  const profileUrl = data.avatar_url;
+  profilePicture.src = profileUrl;
+  // Get user bio
+  if (data.bio) {
+    userBio.textContent = data.bio;
+  }
+}
+
+// Event listeners
+searchBtn.addEventListener("click", searchUser);
+searchInput.addEventListener("input", () => {
+  // Hide error message
+  errorMessage.style.display = "none";
+});
